8951fbf2aef9adc56b461dd6194bcfae8f049d55
Jack Kennedy <125822604+kennay-jks@users.noreply.github.com>
2023-10-02 00:49:14 -0300

    dark-mode

--- js/mode.js
+++ js/mode.js
@@ -1,39 +1,24 @@
-let shadow = document.querySelector('.shadow-mode')
-let body = document.querySelector('body')
-let ball = document.querySelector('.ball')
-let ini = document.querySelector('.inicio')
-let para = document.querySelector('#para')
-let black = document.querySelector('#preto-p')
-let white = document.querySelector('#branco-p')
-let preto = document.querySelector('#preto')
-let branco = document.querySelector('#branco')
+let env = document.querySelector('.env-mode')
 
-shadow.addEventListener('click', () => {
-    if (shadow.classList.contains('active')) {
-        shadow.classList.remove('active')
-        shadow.classList.add('nothing')
-        shadow.style.background = '#080808'
-        body.style.background = '#d6d6d6'
-        body.style.color = '#080808'
-        ball.style.background = '#d6d6d6'
-        ini.style.background = '#080808'
-        para.style.color = '#d6d6d6'
-        branco.style.display = 'none'
-        preto.style.display = 'block'
-        black.style.display = 'none'
-        white.style.display = 'block'
-    } else {
-        shadow.classList.remove('nothing')
-        shadow.classList.add('active')
-        shadow.style.background = '#d6d6d6'
-        body.style.background = '#080808'
-        body.style.color = '#d6d6d6'
-        ball.style.background = '#080808'
-        ini.style.background = '#d6d6d6'
-        para.style.color = '#080808'
-        preto.style.display = 'none'
-        branco.style.display = 'block'
-        white.style.display = 'none'
-        black.style.display = 'block'
+function darkMode() {
+    document.body.classList.toggle('dark')
+}
+
+function loadTheme() {
+    let mode = localStorage.getItem('dark')
+    
+    if (mode) {
+        darkMode()
+    }
+}
+
+loadTheme()
+
+env.addEventListener('click', () => {
+    darkMode()
+
+    localStorage.removeItem('dark')
+    if (document.body.classList.contains('dark')) {
+        localStorage.setItem('dark', 1)
     }
 })
